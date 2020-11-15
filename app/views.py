68185af5bd2179c8891bc5c1de8ad9a121c10b1d
ratpiss <31166611+ratpiss@users.noreply.github.com>
2020-11-15 21:03:46 +0200

    Add files via upload

---
+++ app/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse, HttpResponseRedirect, HttpRequest
+from django.template import loader
+import os
+from dotenv import load_dotenv
+from app import api
+from django.views import View
+import json
+
+load_dotenv()
+
+
+def index(request):
+    template = loader.get_template('app/index.html')
+    context = {
+
+    }
+    if request.method == 'POST':
+        name = str(request.POST['name'])
+        region = str(request.POST['region'])
+        message = 'No info'
+        if name and region:
+            try:
+                data = api.get_summoner(name, region)
+                name = data['name']
+                level = data['level']
+                avatar = data['avatar']
+                context = {
+                    'name': name,
+                    'level': level,
+                    'avatar': avatar,
+                }
+            except:
+                print('No such user')
+
+        return HttpResponse(template.render(context, request))
+    else:
+        return HttpResponse(template.render(context, request))
